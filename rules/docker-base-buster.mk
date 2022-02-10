51716995277a5469ee0ab2364851c34d5693bd0d
Chris Sommers <chrispsommers@gmail.com>
2022-02-10 09:30:12 -0800

    First commit of USDT tracing and tools. sonic-sairedis has USDT probes around SAI calls within VendosSai.cpp. SONiC slaves incloude sdt.h. Debian OS images contain bpfcc tools. Still need to add Linux headers to image in the build, currently has to be done manually on the box.

--- rules/docker-base-buster.mk
+++ rules/docker-base-buster.mk
@@ -11,7 +11,11 @@ VIM = vim
 OPENSSH = openssh-client
 SSHPASS = sshpass
 STRACE = strace
-$(DOCKER_BASE_BUSTER)_DBG_IMAGE_PACKAGES += $(GDB) $(GDBSERVER) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE)
+BINUTILS = binutils # readelf, nm, etc.
+#LINUX_HDRS = linux-headers-5.10.0-10-amd64 linux-headers-5.10.0-10-common linux-headers-amd64
+BPF_TOOLS = bpfcc-tools bpftrace bpftool
+$(DOCKER_BASE_BUSTER)_DBG_IMAGE_PACKAGES += $(GDB) $(GDBSERVER) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE) \
+	$(BINUTILS) $(LINUX_HDRS) $(BPF_TOOLS)
 
 SONIC_DOCKER_IMAGES += $(DOCKER_BASE_BUSTER)
 SONIC_BUSTER_DOCKERS += $(DOCKER_BASE_BUSTER)
--- rules/docker-base-stretch.mk
+++ rules/docker-base-stretch.mk
@@ -10,7 +10,11 @@ VIM = vim
 OPENSSH = openssh-client
 SSHPASS = sshpass
 STRACE = strace
-$(DOCKER_BASE_STRETCH)_DBG_IMAGE_PACKAGES += $(GDB) $(GDBSERVER) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE)
+BINUTILS = binutils # readelf, nm, etc.
+#LINUX_HDRS = linux-headers-5.10.0-10-amd64 linux-headers-5.10.0-10-common linux-headers-amd64
+BPF_TOOLS = bpfcc-tools bpftrace bpftool
+$(DOCKER_BASE_STRETCH)_DBG_IMAGE_PACKAGES += $(GDB) $(GDBSERVER) $(VIM) $(OPENSSH) $(SSHPASS) $(STRACE) \
+	$(BINUTILS) $(LINUX_HDRS) $(BPF_TOOLS)
 
 SONIC_DOCKER_IMAGES += $(DOCKER_BASE_STRETCH)
 SONIC_STRETCH_DOCKERS += $(DOCKER_BASE_STRETCH)
